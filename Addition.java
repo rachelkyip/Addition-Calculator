d33cde70d1c97fc1f2a71f9a098176518e45f603
Rachel Yip <rachel.k.yip@gmail.com>
2016-08-03 20:27:41 -0400

    Simple code for a calculator

---
+++ Addition.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Addition {
+     public static void main(String[] args){
+           @SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+           System.out.println(  in.nextInt() + in.nextInt() );
+     }
+}
